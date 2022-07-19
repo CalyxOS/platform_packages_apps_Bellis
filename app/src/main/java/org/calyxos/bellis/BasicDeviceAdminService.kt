621c7590095c7f3776063aba413b70334e994221
Oliver Scott <olivercscott@gmail.com>
2022-07-19 10:50:16 -0400

    Remove user restriction on installing packages & bluetooth sharing
    Change-Id: I2ad367c94c03cc782ba724d99a6cd5466dd98730

---
+++ app/src/main/java/org/calyxos/bellis/BasicDeviceAdminService.kt
@@ -0,0 +1,31 @@
+package org.calyxos.bellis
+
+import android.app.admin.DeviceAdminService
+import android.app.admin.DevicePolicyManager
+import android.os.UserManager.DISALLOW_BLUETOOTH_SHARING
+import android.os.UserManager.DISALLOW_INSTALL_UNKNOWN_SOURCES
+import androidx.core.content.edit
+
+class BasicDeviceAdminService : DeviceAdminService() {
+
+    private val prefVersion = "version"
+
+    override fun onCreate() {
+        super.onCreate()
+
+        val devicePolicyManager = getSystemService(DevicePolicyManager::class.java)
+        val sharedPreferences = getSharedPreferences(packageName, MODE_PRIVATE)
+        val componentName = BasicDeviceAdminReceiver.getComponentName(this)
+        val version = packageManager.getPackageInfo(packageName, 0).longVersionCode
+
+        if (version < sharedPreferences.getLong(prefVersion, 0)) {
+            devicePolicyManager.apply {
+                clearUserRestriction(componentName, DISALLOW_INSTALL_UNKNOWN_SOURCES)
+                clearUserRestriction(componentName, DISALLOW_BLUETOOTH_SHARING)
+            }
+        }
+
+        sharedPreferences.edit { putLong(prefVersion, version) }
+    }
+
+}
