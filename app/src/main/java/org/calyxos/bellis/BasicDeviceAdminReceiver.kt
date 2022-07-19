2df12177aac10f2daa809be32bf433c5d0bd7175
Oliver Scott <olivercscott@gmail.com>
2022-07-19 10:50:16 -0400

    Remove user restriction on installing packages & bluetooth sharing
    Change-Id: I2ad367c94c03cc782ba724d99a6cd5466dd98730

--- app/src/main/java/org/calyxos/bellis/BasicDeviceAdminReceiver.kt
+++ app/src/main/java/org/calyxos/bellis/BasicDeviceAdminReceiver.kt
@@ -18,12 +18,41 @@ package org.calyxos.bellis
 import android.app.admin.DeviceAdminReceiver
 import android.content.ComponentName
 import android.content.Context
+import android.content.Intent
+import android.os.UserManager.DISALLOW_BLUETOOTH_SHARING
+import android.os.UserManager.DISALLOW_INSTALL_UNKNOWN_SOURCES
+import android.util.Log
 
 class BasicDeviceAdminReceiver : DeviceAdminReceiver() {
+
+    private val TAG = BasicDeviceAdminReceiver::class.java.simpleName
 
     companion object {
         fun getComponentName(context: Context): ComponentName {
             return ComponentName(context, BasicDeviceAdminReceiver::class.java.name)
+        }
+    }
+
+    override fun onReceive(context: Context, intent: Intent) {
+        super.onReceive(context, intent)
+        when (intent.action) {
+            Intent.ACTION_MY_PACKAGE_REPLACED -> {
+                val componentName = getComponentName(context)
+                val version = context.packageManager
+                    .getPackageInfo(context.packageName, 0)
+                    .longVersionCode
+
+                if (version < 200) {
+                    getManager(context).apply {
+                        clearUserRestriction(componentName, DISALLOW_INSTALL_UNKNOWN_SOURCES)
+                        clearUserRestriction(componentName, DISALLOW_BLUETOOTH_SHARING)
+                    }
+                }
+
+            }
+            else -> {
+                Log.d(TAG, "Got an unhandled intent!")
+            }
         }
     }
 
