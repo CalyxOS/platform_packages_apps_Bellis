b32399be14a7be8bf43e1095f7a29d5f0707d325
Aayush Gupta <aayushgupta219@gmail.com>
2022-08-09 23:43:40 +0530

    Bellis: The great kotlin rewrite
    Also import and add ktlint which allows formatting files easily
    
    Signed-off-by: Aayush Gupta <aayushgupta219@gmail.com>
    Change-Id: I2f9efa0828be41e2b7c94c3fa31fd967f1a081f5

---
+++ app/src/main/java/org/calyxos/bellis/SetupProfileFragment.kt
@@ -0,0 +1,54 @@
+/*
+ * Copyright (C) 2022 The Calyx Institute
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.calyxos.bellis
+
+import android.app.admin.DevicePolicyManager
+import android.content.ActivityNotFoundException
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.fragment.app.Fragment
+
+class SetupProfileFragment : Fragment(R.layout.setup_profile_fragment) {
+
+    private val startForResult =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {}
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        view.findViewById<View>(R.id.set_up_profile)
+            .setOnClickListener { provisionManagedProfile(view.context) }
+    }
+
+    private fun provisionManagedProfile(context: Context) {
+        val intent = Intent(DevicePolicyManager.ACTION_PROVISION_MANAGED_PROFILE).apply {
+            val component = BasicDeviceAdminReceiver.getComponentName(context)
+            putExtra(DevicePolicyManager.EXTRA_PROVISIONING_DEVICE_ADMIN_COMPONENT_NAME, component)
+        }
+        try {
+            startForResult.launch(intent)
+        } catch (exception: ActivityNotFoundException) {
+            Toast.makeText(
+                context,
+                context.getString(R.string.managed_provisioning_not_enabled),
+                Toast.LENGTH_SHORT
+            ).show()
+        }
+    }
+}
