3d52e6105d0489d0f2eb2570c8d5c10351ac7dc9
Aayush Gupta <aayushgupta219@gmail.com>
2022-11-01 22:43:13 +0530

    Support admin-integrated provisioning flow
    Additionally, set Orbot as the always-on VPN, prevent uninstallation,
    and allow notifications so the user knows the status of Orbot even if
    they have never opened it manually.
    
    Change-Id: I3f0273af1b28dfd873bea20f9b8605850f671c9c

--- app/src/main/java/org/calyxos/bellis/utils/PostProvisioningHelper.kt
+++ app/src/main/java/org/calyxos/bellis/utils/PostProvisioningHelper.kt
@@ -6,10 +6,14 @@
 
 package org.calyxos.bellis.utils
 
+import android.Manifest.permission.POST_NOTIFICATIONS
+import android.app.Activity
 import android.app.admin.DevicePolicyManager
+import android.app.admin.DevicePolicyManager.PERMISSION_GRANT_STATE_GRANTED
 import android.content.ActivityNotFoundException
 import android.content.Context
 import android.content.Intent
+import android.os.PersistableBundle
 import android.util.Log
 import androidx.work.OneTimeWorkRequestBuilder
 import androidx.work.OutOfQuotaPolicy
@@ -19,9 +23,17 @@ import org.calyxos.bellis.R
 
 object PostProvisioningHelper {
 
-    private const val TAG = "PostProvisioningHelper"
+    private val TAG = PostProvisioningHelper::class.java.simpleName
+
+    private const val GARLIC_LEVEL = "garlic_level"
     private const val PREFS = "post-provisioning"
     private const val PREF_DONE = "done"
+
+    private const val ORBOT_PKG = "org.torproject.android"
+
+    private enum class GarlicLevel {
+        STANDARD, SAFER, SAFEST
+    }
 
     fun completeProvisioning(context: Context) {
         if (!provisioningComplete(context)) {
@@ -35,6 +47,15 @@ object PostProvisioningHelper {
                         OutOfQuotaPolicy.RUN_AS_NON_EXPEDITED_WORK_REQUEST
                     ).build()
                 )
+
+                // Do required setup for Garlic Level
+                if (context is Activity) {
+                    when (getGarlicLevel(context.intent)) {
+                        GarlicLevel.SAFER -> setupSafer(context)
+
+                        else -> Log.i(TAG, "Garlic Level: Standard, nothing to do!")
+                    }
+                }
             }
 
             markComplete(context)
@@ -73,5 +94,40 @@ object PostProvisioningHelper {
                 Log.w(TAG, "SetupWizardActivity not found for $setupWizard, skipping", e)
             }
         }
+    }
+
+    private fun getGarlicLevel(intent: Intent): GarlicLevel {
+        val garlicLevelInt = intent.getParcelableExtra(
+            DevicePolicyManager.EXTRA_PROVISIONING_ADMIN_EXTRAS_BUNDLE,
+            PersistableBundle::class.java
+        )?.getInt(GARLIC_LEVEL, GarlicLevel.STANDARD.ordinal)
+            ?: GarlicLevel.STANDARD.ordinal
+
+        return GarlicLevel.values()[garlicLevelInt]
+    }
+
+    private fun setupOrbot(context: Context) {
+        try {
+            val componentName = BasicDeviceAdminReceiver.getComponentName(context)
+
+            context.getSystemService(DevicePolicyManager::class.java).apply {
+                // Set Orbot as always-on VPN, prevent uninstallation, and allow notifications.
+                setAlwaysOnVpnPackage(componentName, ORBOT_PKG, true)
+                setUninstallBlocked(componentName, ORBOT_PKG, true)
+                setPermissionGrantState(
+                    componentName,
+                    ORBOT_PKG,
+                    POST_NOTIFICATIONS,
+                    PERMISSION_GRANT_STATE_GRANTED
+                )
+            }
+        } catch (exception: Exception) {
+            Log.e(TAG, "Failed to set always-on VPN", exception)
+        }
+    }
+
+    private fun setupSafer(context: Context) {
+        // Do required setup for orbot
+        setupOrbot(context)
     }
 }
