d70df171fb2236fc1af8a0f92ff8a41b222e442f
Aayush Gupta <aayushgupta219@gmail.com>
2022-11-01 22:43:13 +0530

    Support admin-integrated provisioning flow
    Signed-off-by: Aayush Gupta <aayushgupta219@gmail.com>
    Change-Id: I3f0273af1b28dfd873bea20f9b8605850f671c9c

--- app/src/main/java/org/calyxos/bellis/PostProvisioningHelper.kt
+++ app/src/main/java/org/calyxos/bellis/PostProvisioningHelper.kt
@@ -6,19 +6,32 @@
 
 package org.calyxos.bellis
 
+import android.app.Activity
 import android.app.admin.DevicePolicyManager
+import android.content.ActivityNotFoundException
 import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.PersistableBundle
 import android.os.UserManager
+import android.util.Log
 
 object PostProvisioningHelper {
+    private const val ORBOT_PKG = "org.torproject.android"
+    private const val CHROMIUM_PKG = "org.chromium.chrome"
 
-    private const val PREFS = "post-provisioning"
-    private const val PREF_DONE = "done"
-    private val userRestrictions = listOf(
+    private const val GARLIC_LEVEL = "garlic_level"
+
+    private val TAG = PostProvisioningHelper::class.java.simpleName
+
+    // Default managed profile restrictions to clear
+    private val defaultProfileOwnerRestrictions = listOf(
         UserManager.DISALLOW_INSTALL_UNKNOWN_SOURCES,
-        UserManager.DISALLOW_BLUETOOTH_SHARING
+        UserManager.DISALLOW_BLUETOOTH_SHARING,
     )
-    private val requiredPackages = listOf(
+
+    // Default apps to enable on creation of new managed profile
+    private val systemApps = listOf(
         "com.google.android.gms",
         "com.google.android.gsf",
         "com.android.vending",
@@ -27,32 +40,55 @@ object PostProvisioningHelper {
         "org.microg.nlp.backend.nominatim",
         "com.stevesoltys.seedvault",
         "org.fdroid.fdroid",
-        "org.chromium.chrome"
+        CHROMIUM_PKG
     )
 
+    private enum class GarlicLevel {
+        STANDARD, SAFER, SAFEST
+    }
+
     fun completeProvisioning(context: Context) {
-        if (!provisioningComplete(context)) {
-            val devicePolicyManager = context.getSystemService(DevicePolicyManager::class.java)
-            val componentName = BasicDeviceAdminReceiver.getComponentName(context)
-            devicePolicyManager.apply {
+        val devicePolicyManager = context.getSystemService(DevicePolicyManager::class.java)
+        val componentName = BasicDeviceAdminReceiver.getComponentName(context)
+        devicePolicyManager.apply {
+            if (isProfileOwnerApp(componentName.packageName)) {
                 setProfileName(componentName, context.getString(R.string.app_name))
                 setProfileEnabled(componentName)
-
-                // Clear user restrictions
-                userRestrictions.forEach {
-                    devicePolicyManager.clearUserRestriction(componentName, it)
+                defaultProfileOwnerRestrictions.forEach { clearUserRestriction(componentName, it) }
+                systemApps.forEach {
+                    try {
+                        enableSystemApp(componentName, it)
+                    } catch (e: IllegalArgumentException) {
+                        Log.e("PostProvisioningHelper", "Failed to enable $it")
+                    }
                 }
 
-                // Enable required packages
-                requiredPackages.forEach {
-                    devicePolicyManager.enableSystemApp(componentName, it)
+                if (context is Activity) {
+                    val garlicLevel = context.intent.getParcelableExtra(
+                        DevicePolicyManager.EXTRA_PROVISIONING_ADMIN_EXTRAS_BUNDLE,
+                        PersistableBundle::class.java
+                    )?.getInt(GARLIC_LEVEL, 0)
+                    if (garlicLevel != GarlicLevel.STANDARD.ordinal) {
+                        try {
+                            setAlwaysOnVpnPackage(componentName, ORBOT_PKG, true)
+                        } catch (exception: PackageManager.NameNotFoundException) {
+                            Log.e(TAG, "Failed to set always-on VPN", exception)
+                        }
+                    }
+                }
+
+                try {
+                    val intent = Intent(Intent.ACTION_MAIN).apply {
+                        val setupWizard = "org.lineageos.setupwizard"
+                        val setupWizardActivity = ".SetupWizardActivity"
+                        setClassName(setupWizard, setupWizard + setupWizardActivity)
+                        addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+                    }
+                    context.startActivity(intent)
+                } catch (e: ActivityNotFoundException) {
+                    Log.e(TAG, "Failed to start setup wizard")
                 }
             }
         }
-    }
-
-    private fun provisioningComplete(context: Context): Boolean {
-        val sharedPreferences = context.getSharedPreferences(PREFS, Context.MODE_PRIVATE)
-        return sharedPreferences.getBoolean(PREF_DONE, false)
     }
 }
