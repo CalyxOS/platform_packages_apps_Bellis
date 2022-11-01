d162ef118abfe342882a48802cb80bcfda2c5d7a
Aayush Gupta <aayushgupta219@gmail.com>
2022-11-01 22:43:13 +0530

    DO NOT MERGE: SetupActivity: Support device owner provision
    Signed-off-by: Aayush Gupta <aayushgupta219@gmail.com>
    Change-Id: I3f0273af1b28dfd873bea20f9b8605850f671c9c

--- app/src/main/java/org/calyxos/bellis/SetupActivity.kt
+++ app/src/main/java/org/calyxos/bellis/SetupActivity.kt
@@ -17,11 +17,11 @@
 package org.calyxos.bellis
 
 import android.app.Activity
+import android.app.admin.DevicePolicyManager
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 
-// TODO: Make this a broadcast receiver instead
 class SetupActivity : Activity() {
 
     private val setupWizard = "org.lineageos.setupwizard"
@@ -29,9 +29,44 @@ class SetupActivity : Activity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val defaultProvisioningMode = DevicePolicyManager.PROVISIONING_MODE_MANAGED_PROFILE
 
-        PostProvisioningHelper.completeProvisioning(this)
-        navigateBackToFTSW(this)
+        when (intent.action) {
+            DevicePolicyManager.ACTION_GET_PROVISIONING_MODE -> {
+                val allowedProvisioningModes = intent.getIntegerArrayListExtra(
+                    DevicePolicyManager.EXTRA_PROVISIONING_ALLOWED_PROVISIONING_MODES
+                )
+
+                allowedProvisioningModes?.let {
+                    val provisioningMode = if (defaultProvisioningMode in it) {
+                        defaultProvisioningMode
+                    } else {
+                        DevicePolicyManager.PROVISIONING_MODE_FULLY_MANAGED_DEVICE
+                    }
+                    intent.putExtra(DevicePolicyManager.EXTRA_PROVISIONING_MODE, provisioningMode)
+                    setResult(RESULT_OK, intent)
+                    finish()
+                }
+            }
+            DevicePolicyManager.ACTION_ADMIN_POLICY_COMPLIANCE -> {
+                // TODO: Show policy compliance for the provisioning (?)
+                setResult(RESULT_OK, intent)
+                finish()
+            }
+            DevicePolicyManager.ACTION_PROVISIONING_SUCCESSFUL -> {
+                val provisionedMode = intent.getIntExtra(
+                    DevicePolicyManager.EXTRA_PROVISIONING_MODE,
+                    defaultProvisioningMode
+                )
+
+                if (provisionedMode == defaultProvisioningMode) {
+                    PostProvisioningHelper.completeProvisioning(this)
+                    navigateBackToFTSW(this)
+                } else {
+                    // TODO: Handle device owner provision
+                }
+            }
+        }
     }
 
     private fun navigateBackToFTSW(context: Context) {
