7e822bdcafdb721223d0a0f90d899e503cee59b7
Aayush Gupta <aayushgupta219@gmail.com>
2022-11-04 19:23:39 +0530

    BasicDeviceAdminService: Split and guard managed profile logic
    Ensure logic specific to managed profiles is only executed if app is set as profile owner
    
    Signed-off-by: Aayush Gupta <aayushgupta219@gmail.com>
    Change-Id: I2a6cb21700097f1bc38db69f9cc453ed9eba0eec

--- app/src/main/java/org/calyxos/bellis/BasicDeviceAdminService.kt
+++ app/src/main/java/org/calyxos/bellis/BasicDeviceAdminService.kt
@@ -18,75 +18,96 @@ package org.calyxos.bellis
 import android.app.admin.DeviceAdminService
 import android.app.admin.DevicePolicyManager
 import android.content.BroadcastReceiver
+import android.content.ComponentName
 import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
 import android.content.pm.PackageManager
+import android.content.pm.PackageManager.PackageInfoFlags
 import android.os.UserManager.DISALLOW_BLUETOOTH_SHARING
 import android.os.UserManager.DISALLOW_INSTALL_UNKNOWN_SOURCES
-
 import androidx.core.content.edit
 
 class BasicDeviceAdminService : DeviceAdminService() {
 
-    private val prefVersion = "version"
+    private lateinit var devicePolicyManager: DevicePolicyManager
+    private lateinit var componentName: ComponentName
+    private var managedProfile: Boolean = false
 
     override fun onCreate() {
         super.onCreate()
 
-        val devicePolicyManager = getSystemService(DevicePolicyManager::class.java)
-        val sharedPreferences = getSharedPreferences(packageName, MODE_PRIVATE)
-        val componentName = BasicDeviceAdminReceiver.getComponentName(this)
-        val version = packageManager.getPackageInfo(
-            packageName,
-            PackageManager.PackageInfoFlags.of(0)
-        ).longVersionCode
+        componentName = BasicDeviceAdminReceiver.getComponentName(this)
+        devicePolicyManager = getSystemService(DevicePolicyManager::class.java)
+        managedProfile = devicePolicyManager.isProfileOwnerApp(componentName.packageName)
 
-        val packageIntentFilter = IntentFilter().apply {
-            addAction(Intent.ACTION_PACKAGE_ADDED)
-            addAction(Intent.ACTION_PACKAGE_CHANGED)
-            addAction(Intent.ACTION_PACKAGE_REMOVED)
-            addAction(Intent.ACTION_PACKAGE_REPLACED)
-            addDataScheme("package")
-        }
+        // Register Broadcast Receiver for handling additional packages
+        // installed after service was created
+        registerReceivers()
 
-        registerReceiver(object : BroadcastReceiver() {
-            override fun onReceive(context: Context?, intent: Intent?) {
-                if (intent?.getBooleanExtra(Intent.EXTRA_REPLACING, false) == true) {
-                    return
-                }
-                devicePolicyManager?.apply {
-                    setCrossProfilePackages(componentName, packageManager.getInstalledPackages(
-                        PackageManager.PackageInfoFlags.of(PackageManager.GET_PERMISSIONS.toLong()))
-                        .filter{
-                            it?.requestedPermissions?.contains(
-                                android.Manifest.permission.INTERACT_ACROSS_PROFILES) ?: false
-                        }
-                        .map{ it.packageName }.toSet())
-                }
-            }
-        }, packageIntentFilter)
-
-        if (sharedPreferences.getLong(prefVersion, 0) < 102) {
-            devicePolicyManager?.apply {
-                clearUserRestriction(componentName, DISALLOW_INSTALL_UNKNOWN_SOURCES)
-                clearUserRestriction(componentName, DISALLOW_BLUETOOTH_SHARING)
-            }
-        }
-
-        if (sharedPreferences.getLong(prefVersion, 0) < 103) {
-            devicePolicyManager?.apply {
-                setCrossProfileCalendarPackages(componentName, null)
-                setCrossProfilePackages(componentName, packageManager.getInstalledPackages(
-                    PackageManager.PackageInfoFlags.of(PackageManager.GET_PERMISSIONS.toLong()))
-                    .filter{
-                        it?.requestedPermissions?.contains(
-                            android.Manifest.permission.INTERACT_ACROSS_PROFILES) ?: false
-                    }
-                    .map{ it.packageName }.toSet())
-            }
-        }
-
-        sharedPreferences.edit { putLong(prefVersion, version) }
+        // Run required migrations on version upgrade for existing apps
+        onUpgrade()
     }
+
+    private fun registerReceivers() {
+        if (managedProfile) {
+            val packageIntentFilter = IntentFilter().apply {
+                addAction(Intent.ACTION_PACKAGE_ADDED)
+                addAction(Intent.ACTION_PACKAGE_CHANGED)
+                addAction(Intent.ACTION_PACKAGE_REMOVED)
+                addAction(Intent.ACTION_PACKAGE_REPLACED)
+                addDataScheme("package")
+            }
+
+            registerReceiver(object : BroadcastReceiver() {
+                override fun onReceive(context: Context?, intent: Intent?) {
+                    if (intent?.getBooleanExtra(Intent.EXTRA_REPLACING, false) == true) {
+                        return
+                    }
+                    devicePolicyManager.apply {
+                        setCrossProfilePackages(componentName, getCrossProfilePackages())
+                    }
+                }
+            }, packageIntentFilter)
+        }
+    }
+
+    private fun onUpgrade() {
+        val sharedPreferences = getSharedPreferences(packageName, MODE_PRIVATE)
+        val prefVersion = sharedPreferences.getLong("version", 0)
+        val currentVersion = packageManager.getPackageInfo(packageName, PackageInfoFlags.of(0))
+
+        when {
+            prefVersion < 102 -> {
+                if (managedProfile) {
+                    devicePolicyManager.apply {
+                        clearUserRestriction(componentName, DISALLOW_INSTALL_UNKNOWN_SOURCES)
+                        clearUserRestriction(componentName, DISALLOW_BLUETOOTH_SHARING)
+                    }
+                }
+            }
+            prefVersion < 103 -> {
+                if (managedProfile) {
+                    devicePolicyManager.apply {
+                        setCrossProfileCalendarPackages(componentName, null)
+                        setCrossProfilePackages(componentName, getCrossProfilePackages())
+                    }
+                }
+            }
+        }
+
+        sharedPreferences.edit{ putLong("version", currentVersion.longVersionCode) }
+    }
+
+    private fun getCrossProfilePackages(): Set<String> {
+        val installedPackages = packageManager.getInstalledPackages(
+            PackageInfoFlags.of(PackageManager.GET_PERMISSIONS.toLong())
+        )
+        return installedPackages.filter {
+            it?.requestedPermissions?.contains(
+                android.Manifest.permission.INTERACT_ACROSS_PROFILES
+            ) ?: false
+        }.map { it.packageName }.toSet()
+    }
+
 }
