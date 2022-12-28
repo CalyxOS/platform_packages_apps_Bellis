5784a0628a50c3a01fd6f3a954c3d3fa882bc7e9
Aayush Gupta <aayushgupta219@gmail.com>
2022-12-28 15:15:43 +0530

    Set additional restrictions for safest garlic level
    - Disable usb data signaling
    - Disallow installations from unknown sources globally
    - Disallow debugging features (important features are accessible in other-options now)
    - Disable JavaScript JIT in Chromium
    - Set maximum failed attempts before wipe
    - Require strong authentication after timeout
    
    Signed-off-by: Aayush Gupta <aayushgupta219@gmail.com>
    Change-Id: If192fcf5cf2bfc17c89d8b31bf558320df0cd56e

--- app/src/main/java/org/calyxos/bellis/utils/PostProvisioningHelper.kt
+++ app/src/main/java/org/calyxos/bellis/utils/PostProvisioningHelper.kt
@@ -13,12 +13,16 @@ import android.app.admin.DevicePolicyManager.PERMISSION_GRANT_STATE_GRANTED
 import android.content.Context
 import android.content.Intent
 import android.os.PersistableBundle
+import android.os.UserManager.DISALLOW_DEBUGGING_FEATURES
+import android.os.UserManager.DISALLOW_INSTALL_UNKNOWN_SOURCES_GLOBALLY
 import android.util.Log
+import androidx.core.os.bundleOf
 import androidx.work.OneTimeWorkRequestBuilder
 import androidx.work.OutOfQuotaPolicy
 import androidx.work.WorkManager
 import org.calyxos.bellis.BasicDeviceAdminReceiver
 import org.calyxos.bellis.R
+import java.util.concurrent.TimeUnit
 
 object PostProvisioningHelper {
 
@@ -30,6 +34,7 @@ object PostProvisioningHelper {
 
     private const val ORBOT_PKG = "org.torproject.android"
     private const val ORBOT_ACTION_START = "org.torproject.android.intent.action.START"
+    private const val CHROMIUM_PKG = "org.chromium.chrome"
 
     private enum class GarlicLevel {
         STANDARD, SAFER, SAFEST
@@ -52,6 +57,8 @@ object PostProvisioningHelper {
                 if (context is Activity) {
                     when (getGarlicLevel(context.intent)) {
                         GarlicLevel.SAFER -> setupSafer(context)
+
+                        GarlicLevel.SAFEST -> setupSafest(context)
 
                         else -> Log.i(TAG, "Garlic Level: Standard, nothing to do!")
                     }
@@ -120,5 +127,31 @@ object PostProvisioningHelper {
     private fun setupSafer(context: Context) {
         // Do required setup for orbot
         setupOrbot(context)
+    }
+
+    private fun setupSafest(context: Context) {
+        // Do required setup for orbot
+        setupOrbot(context)
+
+        val componentName = BasicDeviceAdminReceiver.getComponentName(context)
+        context.getSystemService(DevicePolicyManager::class.java).apply {
+            // Disable USB data signaling if possible
+            if (canUsbDataSignalingBeDisabled()) isUsbDataSignalingEnabled = false
+
+            // Disable debugging features and app installation from unknown sources
+            addUserRestriction(componentName, DISALLOW_INSTALL_UNKNOWN_SOURCES_GLOBALLY)
+            getParentProfileInstance(componentName)
+                .addUserRestriction(componentName, DISALLOW_DEBUGGING_FEATURES)
+
+            // Wipe profile after 3 failed attempts
+            setMaximumFailedPasswordsForWipe(componentName, 3)
+
+            // Require entering strong auth after 1 hour has passed
+            setRequiredStrongAuthTimeout(componentName, TimeUnit.HOURS.toMillis(1))
+
+            // Disable Javascript JIT in Chromium
+            val bundle = bundleOf("DefaultJavaScriptJitSetting" to 2)
+            setApplicationRestrictions(componentName, CHROMIUM_PKG, bundle)
+        }
     }
 }
