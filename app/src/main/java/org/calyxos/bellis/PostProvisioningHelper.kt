279010dc722dabc43d181659cde0455944bc2452
Tommy Webb <tommy@calyxinstitute.org>
2023-03-15 16:16:39 -0400

    Start Orbot automatically for Safer and Safest
    Orbot 16.6.4-RC-1+ needs to be started just once, and then it will
    start automatically on future boots. After profile provisioning, start
    it in the background using a targeted broadcast, which the app allows
    by default.
    
    Co-authored-by: Aayush Gupta <aayushgupta219@gmail.com>
    Issue: calyxos#1523
    Change-Id: I303ebfedb295dd90983821c0de348133a58bb0a7

--- app/src/main/java/org/calyxos/bellis/PostProvisioningHelper.kt
+++ app/src/main/java/org/calyxos/bellis/PostProvisioningHelper.kt
@@ -24,6 +24,7 @@ object PostProvisioningHelper {
     private const val PREF_DONE = "done"
 
     private const val ORBOT_PKG = "org.torproject.android"
+    private const val ORBOT_ACTION_START = "org.torproject.android.intent.action.START"
     private const val CHROMIUM_PKG = "org.chromium.chrome"
 
     private const val GARLIC_LEVEL = "garlic_level"
@@ -101,6 +102,7 @@ object PostProvisioningHelper {
                         } catch (exception: PackageManager.NameNotFoundException) {
                             Log.e(TAG, "Failed to set always-on VPN", exception)
                         }
+                        context.sendBroadcast(Intent(ORBOT_ACTION_START).setPackage(ORBOT_PKG))
 
                         if (garlicLevel == GarlicLevel.SAFEST.ordinal) {
                             // Set garlic level restrictions
