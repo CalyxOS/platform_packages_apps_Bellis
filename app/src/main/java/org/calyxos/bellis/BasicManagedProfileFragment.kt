7cd204e9a41b9f9d54a90a07c516dc61106c95eb
Tommy Webb <tommy@calyxinstitute.org>
2023-03-16 17:27:53 -0400

    Prevent removing Safest profile
    Track the garlic level that a profile is created with, if any, and
    prevent removing Safest profiles.
    
    Change-Id: I05df9f90aef7ba500c7eb771662b7f7b26b1d5ce

--- app/src/main/java/org/calyxos/bellis/BasicManagedProfileFragment.kt
+++ app/src/main/java/org/calyxos/bellis/BasicManagedProfileFragment.kt
@@ -8,6 +8,7 @@ package org.calyxos.bellis
 
 import android.app.Dialog
 import android.app.admin.DevicePolicyManager
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.os.Process
@@ -23,35 +24,57 @@ class BasicManagedProfileFragment : Fragment(R.layout.basic_managed_profile_frag
     private val TAG = BasicManagedProfileFragment::class.java.simpleName
     private val userSettings = "android.settings.USER_SETTINGS"
 
-    class RemoveProfileDialogFragment : DialogFragment() {
-
-        companion object {
-            fun show(parentFragment: Fragment) {
-                return RemoveProfileDialogFragment().show(
-                    parentFragment.childFragmentManager,
-                    RemoveProfileDialogFragment::class.java.simpleName
-                )
-            }
-        }
+    inner class RemoveProfileDialogFragment : DialogFragment() {
 
         override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
             return MaterialAlertDialogBuilder(requireContext())
                 .setTitle(getString(R.string.remove_profile))
                 .setMessage(getString(R.string.remove_profile_confirmation))
                 .setPositiveButton(android.R.string.ok) { _, _ ->
-                    context?.getSystemService(DevicePolicyManager::class.java)?.wipeData(0)
+                    removeProfile()
                 }
                 .setNegativeButton(android.R.string.cancel, null)
                 .create()
         }
     }
 
+    fun showRemoveProfileDialogFragment() {
+        return RemoveProfileDialogFragment().show(
+            this.childFragmentManager,
+            RemoveProfileDialogFragment::class.java.simpleName
+        )
+    }
+
+    fun shouldAllowRemoveProfile(): Boolean {
+        val context = requireContext()
+        context.getSystemService(DevicePolicyManager::class.java)?.apply {
+            val sharedPreferences = context.getSharedPreferences(context.packageName,
+                Context.MODE_PRIVATE)
+            val garlicLevel = sharedPreferences.getInt("garlicLevel", 0)
+            return garlicLevel < PostProvisioningHelper.GarlicLevel.SAFEST.ordinal
+        }
+        return false
+    }
+
+    fun removeProfile() {
+        if (shouldAllowRemoveProfile()) {
+            context?.getSystemService(DevicePolicyManager::class.java)?.wipeData(0)
+        }
+    }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        view.findViewById<View>(R.id.removeProfile)?.visibility =
+            if (shouldAllowRemoveProfile()) {
+                View.VISIBLE
+            } else {
+                View.INVISIBLE
+            }
+
         view.findViewById<Toolbar>(R.id.toolbar)?.setOnMenuItemClickListener {
             when (it.itemId) {
-                R.id.removeProfile -> RemoveProfileDialogFragment.show(this)
+                R.id.removeProfile -> showRemoveProfileDialogFragment()
                 else -> Log.d(TAG, "Unexpected itemId: ${it.itemId}")
             }
             true
