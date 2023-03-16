285729505c83c807bf46060ac8abdf1cd28ab280
Tommy Webb <tommy@calyxinstitute.org>
2023-03-16 17:27:53 -0400

    Prevent removing Safest profile
    Track the garlic level that a profile is created with, if any, and
    prevent removing Safest profiles.
    
    Issue: calyxos#1518
    Change-Id: I05df9f90aef7ba500c7eb771662b7f7b26b1d5ce

--- app/src/main/java/org/calyxos/bellis/ui/ManagedProfileFragment.kt
+++ app/src/main/java/org/calyxos/bellis/ui/ManagedProfileFragment.kt
@@ -8,6 +8,7 @@ package org.calyxos.bellis.ui
 
 import android.app.Dialog
 import android.app.admin.DevicePolicyManager
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.os.Process
@@ -18,11 +19,19 @@ import androidx.fragment.app.DialogFragment
 import androidx.fragment.app.Fragment
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import org.calyxos.bellis.R
+import org.calyxos.bellis.utils.PostProvisioningHelper
 
 class ManagedProfileFragment : Fragment(R.layout.managed_profile_fragment) {
 
     private val TAG = ManagedProfileFragment::class.java.simpleName
     private val userSettings = "android.settings.USER_SETTINGS"
+
+    companion object {
+        fun shouldAllowRemoveProfile(context: Context): Boolean {
+            return PostProvisioningHelper.getGarlicLevel(context) !=
+                PostProvisioningHelper.GarlicLevel.SAFEST
+        }
+    }
 
     class RemoveProfileDialogFragment : DialogFragment() {
 
@@ -40,7 +49,9 @@ class ManagedProfileFragment : Fragment(R.layout.managed_profile_fragment) {
                 .setTitle(getString(R.string.remove_profile))
                 .setMessage(getString(R.string.remove_profile_confirmation))
                 .setPositiveButton(android.R.string.ok) { _, _ ->
-                    context?.getSystemService(DevicePolicyManager::class.java)?.wipeData(0)
+                    if (shouldAllowRemoveProfile(requireContext())) {
+                        context?.getSystemService(DevicePolicyManager::class.java)?.wipeData(0)
+                    }
                 }
                 .setNegativeButton(android.R.string.cancel, null)
                 .create()
@@ -49,6 +60,13 @@ class ManagedProfileFragment : Fragment(R.layout.managed_profile_fragment) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        view.findViewById<View>(R.id.removeProfile)?.visibility =
+            if (shouldAllowRemoveProfile(view.context)) {
+                View.VISIBLE
+            } else {
+                View.GONE
+            }
 
         view.findViewById<Toolbar>(R.id.toolbar)?.setOnMenuItemClickListener {
             when (it.itemId) {
--- app/src/main/java/org/calyxos/bellis/utils/PostProvisioningHelper.kt
+++ app/src/main/java/org/calyxos/bellis/utils/PostProvisioningHelper.kt
@@ -17,6 +17,7 @@ import android.os.PersistableBundle
 import android.os.UserManager.DISALLOW_DEBUGGING_FEATURES
 import android.os.UserManager.DISALLOW_INSTALL_UNKNOWN_SOURCES_GLOBALLY
 import android.util.Log
+import androidx.core.content.edit
 import androidx.core.os.bundleOf
 import androidx.work.OneTimeWorkRequestBuilder
 import androidx.work.OutOfQuotaPolicy
@@ -29,15 +30,17 @@ object PostProvisioningHelper {
 
     private val TAG = PostProvisioningHelper::class.java.simpleName
 
-    private const val GARLIC_LEVEL = "garlic_level"
     private const val PREFS = "post-provisioning"
+    private const val PREFS_GARLIC = "garlic_preferences"
+
+    private const val GARLIC_LEVEL = "garlic_level"
     private const val PREF_DONE = "done"
 
     private const val ORBOT_PKG = "org.torproject.android"
     private const val ORBOT_ACTION_START = "org.torproject.android.intent.action.START"
     private const val CHROMIUM_PKG = "org.chromium.chrome"
 
-    private enum class GarlicLevel {
+    enum class GarlicLevel {
         STANDARD, SAFER, SAFEST
     }
 
@@ -75,6 +78,16 @@ object PostProvisioningHelper {
         }
     }
 
+    /**
+     * Gets garlic level stored in the application's sharedpref
+     * To read the value from intent, use the overloaded method that takes Intent as param
+     */
+    fun getGarlicLevel(context: Context): GarlicLevel {
+        val sharedPreferences = context.getSharedPreferences(PREFS_GARLIC, Context.MODE_PRIVATE)
+        val garlicLevelInt = sharedPreferences.getInt(GARLIC_LEVEL, GarlicLevel.STANDARD.ordinal)
+        return GarlicLevel.values()[garlicLevelInt]
+    }
+
     private fun provisioningComplete(context: Context): Boolean {
         val sharedPreferences = context.getSharedPreferences(PREFS, Context.MODE_PRIVATE)
         return sharedPreferences.getBoolean(PREF_DONE, false)
@@ -105,6 +118,11 @@ object PostProvisioningHelper {
         return GarlicLevel.values()[garlicLevelInt]
     }
 
+    private fun saveGarlicLevel(garlicLevel: GarlicLevel, context: Context) {
+        val sharedPreferences = context.getSharedPreferences(PREFS_GARLIC, Context.MODE_PRIVATE)
+        sharedPreferences.edit { putInt(GARLIC_LEVEL, garlicLevel.ordinal) }
+    }
+
     private fun setupSafer(dpm: DevicePolicyManager, componentName: ComponentName, ctx: Context) {
         try {
             // Set Orbot as always-on-vpn and start it
@@ -120,6 +138,9 @@ object PostProvisioningHelper {
             ctx.sendBroadcast(Intent(ORBOT_ACTION_START).setPackage(ORBOT_PKG))
         } catch (exception: Exception) {
             Log.e(TAG, "Failed to set always-on VPN", exception)
+        } finally {
+            // Save garlic level
+            saveGarlicLevel(GarlicLevel.SAFER, ctx)
         }
     }
 
@@ -146,5 +167,8 @@ object PostProvisioningHelper {
             val bundle = bundleOf("DefaultJavaScriptJitSetting" to 2)
             setApplicationRestrictions(componentName, CHROMIUM_PKG, bundle)
         }
+
+        // Save garlic level
+        saveGarlicLevel(GarlicLevel.SAFEST, ctx)
     }
 }
