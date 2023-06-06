6b78988e6c186a79999bb92f6ff54cec2d434c72
Tommy Webb <tommy@calyxinstitute.org>
2023-06-06 10:47:11 -0400

    Prevent a hang prior to work profile Setup Wizard
    Rework the handling of system apps. Use WorkManager to enable them
    in the background. Otherwise, the screen may remain blank for an
    extended period, and the device may become temporarily unresponsive.
    
    Co-authored-by: Oliver Scott <olivercscott@gmail.com>
    Issue: calyxos#1705
    Change-Id: I2474d8770c600b9afe72b4be25af2616253d78e8

--- app/src/main/java/org/calyxos/bellis/PostProvisioningHelper.kt
+++ app/src/main/java/org/calyxos/bellis/PostProvisioningHelper.kt
@@ -10,6 +10,11 @@ import android.app.admin.DevicePolicyManager
 import android.content.Context
 import android.os.UserManager
 import android.util.Log
+import androidx.work.OneTimeWorkRequestBuilder
+import androidx.work.OutOfQuotaPolicy
+import androidx.work.WorkManager
+import androidx.work.Worker
+import androidx.work.WorkerParameters
 
 object PostProvisioningHelper {
 
@@ -20,7 +25,9 @@ object PostProvisioningHelper {
         UserManager.DISALLOW_INSTALL_UNKNOWN_SOURCES,
         UserManager.DISALLOW_BLUETOOTH_SHARING
     )
-    private val requiredPackages = listOf(
+
+    // Default apps to enable on creation of new managed profile
+    private val systemApps = listOf(
         "com.android.gallery3d",
         "com.android.vending",
         "com.android.camera2",
@@ -35,6 +42,23 @@ object PostProvisioningHelper {
         "org.microg.nlp.backend.nominatim"
     )
 
+    class SystemAppWorker(private val context: Context, workerParams: WorkerParameters) :
+        Worker(context, workerParams) {
+        override fun doWork(): Result {
+            val componentName = BasicDeviceAdminReceiver.getComponentName(context)
+            context.getSystemService(DevicePolicyManager::class.java)?.apply {
+                systemApps.forEach {
+                    try {
+                        enableSystemApp(componentName, it)
+                    } catch (e: IllegalArgumentException) {
+                        Log.e(TAG, "Failed to enable $it")
+                    }
+                }
+            }
+            return Result.success()
+        }
+    }
+
     fun completeProvisioning(context: Context) {
         if (!provisioningComplete(context)) {
             val devicePolicyManager = context.getSystemService(DevicePolicyManager::class.java)
@@ -43,18 +67,15 @@ object PostProvisioningHelper {
                 setProfileName(componentName, context.getString(R.string.app_name))
                 setProfileEnabled(componentName)
 
+                WorkManager.getInstance(context).enqueue(
+                    OneTimeWorkRequestBuilder<SystemAppWorker>().setExpedited(
+                        OutOfQuotaPolicy.RUN_AS_NON_EXPEDITED_WORK_REQUEST
+                    ).build()
+                )
+
                 // Clear user restrictions
                 userRestrictions.forEach {
                     devicePolicyManager.clearUserRestriction(componentName, it)
-                }
-
-                // Enable required packages
-                requiredPackages.forEach {
-                    try {
-                        devicePolicyManager.enableSystemApp(componentName, it)
-                    } catch (exception: Exception) {
-                        Log.i(TAG, "Failed to enable: $it", exception)
-                    }
                 }
             }
         }
