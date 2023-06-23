f5cdcbe5bdc157379ea197f849f84152dd44f0fd
Chirayu Desai <chirayudesai1@gmail.com>
2023-06-24 01:05:40 +0530

    Don't clear user restrictions twice
    * These are already handled in BasicAdminService
      which does migration + defaults
    
    Change-Id: I43a1dc55f104d011f8dff3f39141c4eb8953a15b

--- app/src/main/java/org/calyxos/bellis/PostProvisioningHelper.kt
+++ app/src/main/java/org/calyxos/bellis/PostProvisioningHelper.kt
@@ -21,10 +21,6 @@ object PostProvisioningHelper {
     private val TAG = PostProvisioningHelper::class.java.simpleName
     private const val PREFS = "post-provisioning"
     private const val PREF_DONE = "done"
-    private val userRestrictions = listOf(
-        UserManager.DISALLOW_INSTALL_UNKNOWN_SOURCES,
-        UserManager.DISALLOW_BLUETOOTH_SHARING
-    )
 
     // Default apps to enable on creation of new managed profile
     private val systemApps = listOf(
@@ -72,11 +68,6 @@ object PostProvisioningHelper {
                         OutOfQuotaPolicy.RUN_AS_NON_EXPEDITED_WORK_REQUEST
                     ).build()
                 )
-
-                // Clear user restrictions
-                userRestrictions.forEach {
-                    devicePolicyManager.clearUserRestriction(componentName, it)
-                }
             }
 
             val sharedPreferences = context.getSharedPreferences(PREFS, Context.MODE_PRIVATE)
