de92013d5feb494536492d262b672e0f63eea3fe
Aayush Gupta <aayushgupta219@gmail.com>
2023-07-28 00:44:36 +0700

    BasicManagedProfileFragment: Only show manage options if provisioningComplete
    Signed-off-by: Aayush Gupta <aayushgupta219@gmail.com>
    Change-Id: Ia8a8a592a89aaa53fc834893630fe8afbc260f98

--- app/src/main/java/org/calyxos/bellis/PostProvisioningHelper.kt
+++ app/src/main/java/org/calyxos/bellis/PostProvisioningHelper.kt
@@ -78,7 +78,7 @@ object PostProvisioningHelper {
         }
     }
 
-    private fun provisioningComplete(context: Context): Boolean {
+    fun provisioningComplete(context: Context): Boolean {
         val sharedPreferences = context.getSharedPreferences(PREFS, Context.MODE_PRIVATE)
         return sharedPreferences.getBoolean(PREF_DONE, false)
     }
