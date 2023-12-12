27ea4cc3b7807940319fd77757ab3c51162143fd
Aayush Gupta <aayushgupta219@gmail.com>
2023-12-12 20:12:08 +0530

    Bump to latest stable AGP & Gradle versions
    Signed-off-by: Aayush Gupta <aayushgupta219@gmail.com>
    Change-Id: Idaff31bc2d4a86f0806fc8a51c936cd88f557ef7

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@
  */
 
 plugins {
-    id("com.android.application") version "8.1.3" apply false
+    id("com.android.application") version "8.2.0" apply false
     id("org.jetbrains.kotlin.android") version "1.8.10" apply false
     id("org.jlleitschuh.gradle.ktlint") version "11.6.1" apply false
 }
