470674906dfa06d4ff6721000cc905f364609391
Aayush Gupta <aayushgupta219@gmail.com>
2024-03-13 11:09:45 +0530

    gradle: Bump dependencies version as in AOSP QPR2
    Signed-off-by: Aayush Gupta <aayushgupta219@gmail.com>
    Change-Id: Ic62942e68aab5ace0743795b70ff35b2721d669f

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,8 @@
  */
 
 plugins {
-    id("com.android.application") version "8.2.0" apply false
-    id("org.jetbrains.kotlin.android") version "1.8.10" apply false
+    id("com.android.application") version "8.3.0" apply false
+    // https://android.googlesource.com/platform/external/kotlinc/+/refs/tags/android-14.0.0_r29/build.txt
+    id("org.jetbrains.kotlin.android") version "1.9.0" apply false
     id("org.jlleitschuh.gradle.ktlint") version "11.6.1" apply false
 }
