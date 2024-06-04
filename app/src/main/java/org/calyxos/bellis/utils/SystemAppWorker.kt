83cd783740362589d5ed7614cfd23c13376d971a
Michael Bestas <mkbestas@gmail.com>
2024-06-04 03:23:30 +0300

    Add Etar to system apps
    Issue: calyxos#2362
    Change-Id: I5a5d73ceed2db56421973097c6df50c30c039138

--- app/src/main/java/org/calyxos/bellis/utils/SystemAppWorker.kt
+++ app/src/main/java/org/calyxos/bellis/utils/SystemAppWorker.kt
@@ -26,6 +26,7 @@ class SystemAppWorker(private val context: Context, workerParams: WorkerParamete
         "com.google.android.gsf",
         "com.stevesoltys.seedvault",
         "org.calyxos.eleven",
+        "org.calyxos.etar",
         "org.calyxos.glimpse",
         "org.chromium.chrome"
     )
