ef568cfe8d39bec91895b0105e269bdd336a944f
Oliver Scott <olivercscott@gmail.com>
2024-06-11 16:48:11 -0400

    Remove FakeStore from required packages
    Change-Id: I40f6b624ec1aff6641a58ce5f71f6638537ba3b7

--- app/src/main/java/org/calyxos/bellis/utils/SystemAppWorker.kt
+++ app/src/main/java/org/calyxos/bellis/utils/SystemAppWorker.kt
@@ -20,7 +20,6 @@ class SystemAppWorker(private val context: Context, workerParams: WorkerParamete
     // Default apps to enable on creation of new managed profile
     private val systemApps = listOf(
         "com.android.gallery3d",
-        "com.android.vending",
         "com.android.camera2",
         "com.google.android.gms",
         "com.google.android.gsf",
