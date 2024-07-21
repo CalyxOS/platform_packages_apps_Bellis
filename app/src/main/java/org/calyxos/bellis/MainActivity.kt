943d47c56a848a0131b3fab52b841d439fe11cdf
Oliver Scott <olivercscott@gmail.com>
2024-07-21 01:19:38 -0400

    fixup! Support admin-integrated provisioning flow
    Issue: calyxos#2022
    Change-Id: I04418dc4c20d00bee1eb0dc3a5e6c8e91f6028de

--- app/src/main/java/org/calyxos/bellis/MainActivity.kt
+++ app/src/main/java/org/calyxos/bellis/MainActivity.kt
@@ -39,6 +39,7 @@ class MainActivity : AppCompatActivity() {
             }
             DevicePolicyManager.ACTION_PROVISIONING_SUCCESSFUL -> {
                 PostProvisioningHelper.completeProvisioning(this)
+                PostProvisioningHelper.launchSUW(this)
             }
         }
 
